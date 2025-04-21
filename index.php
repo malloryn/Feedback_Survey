d31ff02cd55ec3333198365c02543f3c2236d755
Mallory Newberry <malloryjo0101@gmail.com>
2025-04-21 16:54:52 -0400

    trying php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $question1 = $_POST['question1'];
+    $question2 = $_POST['question2'];
+
+    $file = fopen("responses.csv", "a");
+    fputcsv($file, [$question1, $question2]);
+    fclose($file);
+
+    echo json_encode(["message" => "Response recorded!"]);
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,11 +20,11 @@
 </head>
 <body>
     <h1>Anonymous Survey</h1>
-    <form action="/submit" method="post">
+    <form action="index.php" method="post">
         <label for="question1">Question 1:</label>
         <input type="text" id="question1" name="question1"><br><br>
         <label for="question2">Question 2:</label>
-        <input type="text" id="question2" name="question2"><br><br>
+       ="text" id="question2" name="question2"><br><br>
         <input type="submit" value="Submit">
     </form>
 </body>
